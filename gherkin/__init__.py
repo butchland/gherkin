fe84f6d74129b511ae8e6d5287e61de86beb2957
Lincoln de Sousa <lincoln@comum.org>
2013-08-11 11:35:16 -0400

    Supporting comments

--- gherkin/__init__.py
+++ gherkin/__init__.py
@@ -15,11 +15,12 @@ class Lexer(object):
 
             # Expressions to find out wtf is that chunk
             handlers = [
-                self.scan_identifier,
-                self.scan_indent,
                 self.scan_dedent,
+                self.scan_indent,
+                self.scan_comment,
+                self.scan_identifier,
+                self.scan_step,
                 self.scan_text,
-                self.scan_newline,
             ]
 
             for handler in handlers:
@@ -44,54 +45,61 @@ class Lexer(object):
 
         return tokens
 
-    def _exec(self, pattern, chunk):
-        found = re.compile(pattern).findall(chunk)
+    def _exec(self, pattern, chunk, flags=0):
+        found = re.compile(pattern, flags=flags).findall(chunk)
         return found and found[0]
 
+    def scan_comment(self, chunk):
+        found = self._exec(r'\A(\s*\#\s*([^\n]+))', chunk)
+        if found:
+            actual, filtered = found
+            return ('comment', filtered, len(actual))
+
     def scan_identifier(self, chunk):
-        found = self._exec(r'\A([^\:\n]+)\:(\s*)', chunk)
-        return found and ('identifier', found[0], sum(map(len, found)) + 1)
+        found = self._exec(r'\A([^\:\n]+)(\: *)', chunk)
+        return found and ('identifier', found[0], sum(map(len, found)))
+
+    def scan_step(self, chunk):
+        found = self._exec(r'\A(Given|When|Then|And) *([^\n\#]+)', chunk)
+        if found:
+            name, text = found
+            return ('step', (name, text), sum(map(len, found))+1)
 
     def scan_text(self, chunk):
-        found = self._exec(r'\A([^\n]+)', chunk)
-        return found and ('text', found, len(found))
-
-    def scan_newline(self, chunk):
-        found = self._exec(r'\n$', chunk)
-        return found and ('', '', len(found))
+        found = self._exec(r'\A([^\n\#]+)', chunk)
+        # Corner case? When there's a comment in the same line we have text we
+        # need to strip the text
+        return found and ('text', found.strip(), len(found))
 
     def scan_dedent(self, chunk):
-        found = self._exec(r'\A(\n)[^ ]', chunk)
+        found = self._exec(r'\A(\n)[^ \#\n]', chunk)
         if found and self.current_indent_size > 0:
-            self.current_indent_size = 0
-            self.indent_stack = []
+            self.current_indent_size -= len(self.indent_stack.pop())
             return ('dedent', found, len(found))
 
     def scan_indent(self, chunk):
         # Don't use `\s` here, since it contains `\n` on it
-        found = self._exec(r'\A\n( +)', chunk)
+        found = self._exec(r'\A(\n)( *)', chunk)
 
         if found:
-
             # Alright, we're inside of an indentation, let's see if it's bigger
-            # or smaller than the current one
-            new_indent = found
-            new_indent_size = len(found)
+            # or smaller than the current one;
+
+            new_line, new_indent = found
+            new_indent_size = len(new_indent)
+            chars_to_eat = new_indent_size + 1  # \n
+
+            # Handling new lines
+            if not new_indent_size:
+                return ('', '', chars_to_eat)
 
             # Handling same level indentation
-            if new_indent_size == self.current_indent_size:
-                return ('', '', new_indent_size + 1)
-
-            # Handling deindentation
-            elif new_indent_size < self.current_indent_size:
-                while len(found) < self.current_indent_size:
-                    self.current_indent_size -= len(self.indent_stack.pop())
-                return ('dedent', found, len(found))
+            elif new_indent_size == self.current_indent_size:
+                return ('', '', chars_to_eat)
 
             # Handling new indentation level
-            self.indent_stack.append(new_indent)
-            self.current_indent_size += new_indent_size
+            self.indent_stack.append(
+                ' ' * (new_indent_size - self.current_indent_size))
+            self.current_indent_size = new_indent_size
 
-            # This `+ 1` is present in the next line cause the '\n' is not part
-            # of the re group
-            return ('indent', self.current_indent_size, len(new_indent) + 1)
+            return ('indent', self.current_indent_size, chars_to_eat)
--- tests/unit/test_gherkin.py
+++ tests/unit/test_gherkin.py
@@ -24,12 +24,40 @@ def test_lexer_single_feature():
     ])
 
 
+def test_lexer_feature_with_comments():
+    # Given that I have a tasty instance of a delicious localized lexer
+    lexer = Lexer('en')
+
+    # When I scan a feature description with comments
+    nodes = lexer.scan('''\
+# Some nice comments
+# Some more nice comments
+
+Feature: Name     # More comments
+  Description     # Desc
+''')
+
+    # Then I see that the corresponding node list is correct
+    nodes.should.equal([
+        ('comment', "Some nice comments"),
+        ('comment', "Some more nice comments"),
+        ('identifier', 'Feature'),
+        ('text', 'Name'),
+        ('comment', 'More comments'),
+        ('indent', 2),
+        ('text', 'Description'),
+        ('comment', 'Desc'),
+        ('dedent', '\n'),
+    ])
+
+
 def test_lexer_two_features():
     # Given that I have an instance of a localized lexer
     lexer = Lexer('en')
 
     # When I scan a feature description
-    nodes = lexer.scan('''Feature: My Feature
+    nodes = lexer.scan('''\
+Feature: My Feature
   Description of my feature in
   multiple lines
 
@@ -55,5 +83,38 @@ Feature: Another feature
         ('text', 'With another'),
         ('text', 'multiline'),
         ('text', 'description!'),
+        ('dedent', '\n'),
+    ])
+
+
+def test_lexer_feature_with_background():
+    # Given that I have a tasty instance of a delicious localized lexer
+    lexer = Lexer('en')
+
+    # When I scan a feature description with a background
+    nodes = lexer.scan('''\
+Feature: Random
+  Description with
+  two lines
+
+  Background:
+    Given a feature
+    And a background
+    And some cookies
+''')
+
+    # Then I see that the corresponding node list is correct
+    nodes.should.equal([
+        ('identifier', 'Feature'),
+        ('text', 'Random'),
+        ('indent', 2),
+        ('text', 'Description with'),
+        ('text', 'two lines'),
+        ('identifier', 'Background'),
+        ('indent', 4),
+        ('step', ('Given', 'a feature')),
+        ('step', ('And', 'a background')),
+        ('step', ('And', 'some cookies')),
+        ('dedent', '\n'),
         ('dedent', '\n'),
     ])
